56ba432aab13b0024555c9181aba92f762a172e2
Charly Gomez <charly.gomez1310@gmail.com>
2022-10-06 21:42:53 +0200

    feat: users service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async getUserById(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+}
