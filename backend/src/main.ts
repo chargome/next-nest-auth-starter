5522d8f9fad5dec6d3c788b1789de0b89958afab
Charly Gomez <charly.gomez1310@gmail.com>
2022-10-14 17:43:14 +0200

    feat: new port + sesssion options

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { credentials: true, origin: 'http://localhost:3000' },
+  });
 
   // auth
   app.use(
@@ -13,7 +15,11 @@ async function bootstrap() {
       secret: 'my-secret', // todo: use env
       resave: false,
       saveUninitialized: false,
-      cookie: { secure: false }, // todo: set to true in prod
+      cookie: {
+        secure: false, // todo: set to true in prod
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24,
+      },
     }),
   );
   app.use(passport.initialize());
@@ -24,6 +30,6 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
 
   // start
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
